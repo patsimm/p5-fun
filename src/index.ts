68b67ff48beee0260688cdb6be630b4bf1469c20
Patrick Simmelbauer <p.simmelbauer@salt-and-pepper.eu>
2017-11-12 20:26:48 +0100

    Initial commit during project setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express'
+const app = express()
+
+const port = 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log('Example app listening on port !'))
