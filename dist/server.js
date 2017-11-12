05a800f7db1c8a63d022f5cdd047fe72439de6e9
patsimm <patrick.simmelbauer@gmail.com>
2017-11-13 00:23:40 +0100

    Remove dist folder from repo

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.use(express.static('dist/www'));
-app.get('/', function (req, res) {
-    res.sendfile('./dist/www/index.html');
-});
-app.listen(5000);
